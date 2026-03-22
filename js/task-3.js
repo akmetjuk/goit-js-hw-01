3e388f85da8d7bbd6444de8a34c81ea387438697
AK <akmetjuk@gmail.com>
2026-03-22 09:14:16 +0200

    - fix in 3rd tas ParseInt -> ParseFloat. - fix formatting

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,11 @@
-function getElementWidth(content, padding, border) 
-{
-    const _content = parseInt(content)
-    const _padding = parseInt(padding)
-    const _border = parseInt(border)
-    return _content + ((_padding + _border) * 2)
+function getElementWidth(content, padding, border) {
+  const _content = parseFloat(content);
+  const _padding = parseFloat(padding);
+  const _border = parseFloat(border);
+  return _content + (_padding + _border) * 2;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(getElementWidth('60px', '12.4px', '8.35px')); // 101,5
